c7cd5e51c901e217c4c278e72862b3f576f855fc
msxzzyc <10168363742qq.com>
2017-07-03 23:22:56 +0800

    添加menuItem
    自定义uimenucontroller的操作，注意点击item默认会调用控制器的方法，故操作的实现方法放在控制器中。

--- MenuController/MenuController/ViewController.m
+++ MenuController/MenuController/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "ZYCLabel.h"
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet ZYCLabel *label;
 
 @end
 
@@ -19,9 +20,47 @@
     [super viewDidLoad];
     
     [self.webView loadHTMLString:@"<div>哈哈哈</.div>" baseURL:nil];
+    
+    [self.label addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(labelClick)]];
 }
 
 
+
+- (void)labelClick
+{
+    
+    
+    //1.label要成为第一响应者(告诉menuController支持哪些操作，这些操作如何处理)
+    [self.label becomeFirstResponder];
+    //2.显示menuController
+    UIMenuController * menu = [UIMenuController sharedMenuController];
+    //targetRect:menuController需要指向的矩形框
+    //tagetView:targetRect会以tagetView的左上角为坐标原点
+    //    [menu setTargetRect:self.frame inView:self.superview];
+    
+    //添加menuItem（点击item默认会调用控制器的方法）
+    UIMenuItem *ding = [[UIMenuItem alloc]initWithTitle:@"顶" action:@selector(ding:)];
+    UIMenuItem *reply = [[UIMenuItem alloc]initWithTitle:@"回复" action:@selector(reply:)];
+    UIMenuItem *report = [[UIMenuItem alloc]initWithTitle:@"举报" action:@selector(report:)];
+    menu.menuItems = @[ding,reply,report];
+    
+    [menu setTargetRect:self.label.bounds inView:self.label];
+    [menu setMenuVisible:YES animated:YES];
+
+}
+     
+- (void)ding:(UIMenuController *)menu
+{
+    NSLog(@"%s",__func__);
+}
+- (void)reply:(UIMenuController *)menu
+{
+    NSLog(@"%s",__func__);
+}
+- (void)report:(UIMenuController *)menu
+{
+    NSLog(@"%s",__func__);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- MenuController/MenuController/ZYCLabel.m
+++ MenuController/MenuController/ZYCLabel.m
@@ -29,9 +29,10 @@
 
 {
     self.userInteractionEnabled = YES;
-    [self addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(labelClick)]];
+//    [self addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(labelClick)]];
     
-}//使label有资格成为第一响应者
+}
+//使label有资格成为第一响应者
 - (BOOL)canBecomeFirstResponder
 {
     
@@ -44,47 +45,54 @@
  */
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender
 {
-    if (action == @selector(cut:)||action == @selector(copy:)||action == @selector(paste:)) return YES;
+//    if (action == @selector(cut:)||action == @selector(copy:)||action == @selector(paste:)) return YES;
     return NO;
     
 }
-- (void)cut:(UIMenuController *)menu
-{
-    //将自己的文字保存到粘贴板
-    [self copy:menu];
-    //清空文字
-    self.text = nil;
-    
-    
-    
-}
-- (void)copy:(UIMenuController *)menu
-{
-    //将自己的文字保存到粘贴板
-    UIPasteboard *board = [UIPasteboard generalPasteboard];
-    board.string = self.text;
-    
-    
-}
-- (void)paste:(UIMenuController *)menu
-{
-    
-    //将粘贴板的文字复制到自己上
-    UIPasteboard *board = [UIPasteboard generalPasteboard];
-    self.text = board.string;
-    
-}
-- (void)labelClick
-{
-    //1.label要成为第一响应者(告诉menuController支持哪些操作，这些操作如何处理)
-    [self becomeFirstResponder];
-    //2.显示menuController
-    UIMenuController * menu = [UIMenuController sharedMenuController];
-    //targetRect:menuController需要指向的矩形框
-    //tagetView:targetRect会以tagetView的左上角为坐标原点
-//    [menu setTargetRect:self.frame inView:self.superview];
-    [menu setTargetRect:self.bounds inView:self];
-    
-    [menu setMenuVisible:YES animated:YES];
-}
+//- (void)cut:(UIMenuController *)menu
+//{
+//    //将自己的文字保存到粘贴板
+//    [self copy:menu];
+//    //清空文字
+//    self.text = nil;
+//    
+//    
+//    
+//}
+//- (void)copy:(UIMenuController *)menu
+//{
+//    //将自己的文字保存到粘贴板
+//    UIPasteboard *board = [UIPasteboard generalPasteboard];
+//    board.string = self.text;
+//    
+//    
+//}
+//- (void)paste:(UIMenuController *)menu
+//{
+//    
+//    //将粘贴板的文字复制到自己上
+//    UIPasteboard *board = [UIPasteboard generalPasteboard];
+//    self.text = board.string;
+//    
+//}
+//- (void)labelClick
+//{
+//    //1.label要成为第一响应者(告诉menuController支持哪些操作，这些操作如何处理)
+//    [self becomeFirstResponder];
+//    //2.显示menuController
+//    UIMenuController * menu = [UIMenuController sharedMenuController];
+//    //targetRect:menuController需要指向的矩形框
+//    //tagetView:targetRect会以tagetView的左上角为坐标原点
+////    [menu setTargetRect:self.frame inView:self.superview];
+//    
+//    //自定义menuItem
+//    UIMenuItem *ding = [[UIMenuItem alloc]initWithTitle:@"顶" action:@selector(ding:)];
+//    UIMenuItem *reply = [[UIMenuItem alloc]initWithTitle:@"回复" action:@selector(reply:)];
+//    UIMenuItem *report = [[UIMenuItem alloc]initWithTitle:@"举报" action:@selector(report:)];
+//    menu.menuItems = @[ding,reply,report];
+//    
+//    [menu setTargetRect:self.bounds inView:self];
+//    [menu setMenuVisible:YES animated:YES];
+//}
+
 @end
